79ea403ee71fa04fbf002edbd58435fc86e0de8c
flhar <flavien.hardy@smile.fr>
2016-03-18 10:13:12 +0100

    MAJ DL angularJS book

--- routes/defaultRouter.js
+++ routes/defaultRouter.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET angularJSPDF */
+app.get('/angularjs', function(req, res){
+
+  var file = '/opt/ninjaSquad_AngularJS.pdf';
+
+  var filename = path.basename(file);
+  var mimetype = mime.lookup(file);
+
+  res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+  res.setHeader('Content-type', mimetype);
+
+  var filestream = fs.createReadStream(file);
+  filestream.pipe(res);
+});
+
 /* GET home page. */
 router.use(function(req, res, next) {
   res.json({
