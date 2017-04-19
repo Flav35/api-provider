d716d2ba81233399782973447149eb535d900b7b
Flav35 <Flav35@users.noreply.github.com>
2017-04-20 00:11:50 +0200

    Update defaultRouter.js

--- routes/defaultRouter.js
+++ routes/defaultRouter.js
@@ -3,21 +3,6 @@ var router = express.Router();
 var path = require('path');
 var mime = require('mime');
 
-/* GET angularJSPDF */
-router.get('/angularjs', function(req, res){
-
-  var file = '/opt/ninjaSquad_AngularJS.pdf';
-
-  var filename = path.basename(file);
-  var mimetype = mime.lookup(file);
-
-  res.setHeader('Content-disposition', 'attachment; filename=' + filename);
-  res.setHeader('Content-type', mimetype);
-
-  var filestream = fs.createReadStream(file);
-  filestream.pipe(res);
-});
-
 /* GET home page. */
 router.use(function(req, res, next) {
   res.json({
