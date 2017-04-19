d392eb3410f432b1aa50739161f73e5e253ba423
Flav35 <Flav35@users.noreply.github.com>
2017-04-20 00:12:06 +0200

    Update defaultRouter.js

--- routes/defaultRouter.js
+++ routes/defaultRouter.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-var mime = require('mime');
 
 /* GET home page. */
 router.use(function(req, res, next) {
